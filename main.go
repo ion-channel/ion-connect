4cce24406f8b6b445894e13a7d3c02be22ddabfc
kellyp <kellyplummer@mac.com>
2015-11-24 15:26:28 -0800

    adding file license header and updating the api header field

--- main.go
+++ main.go
@@ -1,3 +1,10 @@
+// main.go
+//
+// Copyright (C) 2015 Selection Pressure LLC
+//
+// This software may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+
 package main
 
 import (
