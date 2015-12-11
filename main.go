15400b5a122c21ce4e07a0ade0294c11f43262c9
Kit Plummer <kitplummer@gmail.com>
2015-12-11 07:01:05 -0700

    version bump to check the rolling update of rpm

--- main.go
+++ main.go
@@ -8,79 +8,77 @@
 package main
 
 import (
-  "fmt"
-  "github.com/ion-channel/ion-connect/Godeps/_workspace/src/github.com/codegangsta/cli"
-  "github.com/ion-channel/ion-connect/lib"
-  "os"
+	"fmt"
+	"github.com/ion-channel/ion-connect/Godeps/_workspace/src/github.com/codegangsta/cli"
+	"github.com/ion-channel/ion-connect/lib"
+	"os"
 )
 
-
 func main() {
-  app := cli.NewApp()
-  app.Name = "ionconnect"
-  app.Usage = "Control AWS profiles"
-  app.Version = "0.1"
+	app := cli.NewApp()
+	app.Name = "ionconnect"
+	app.Usage = "Control AWS profiles"
+	app.Version = "0.1.1"
 
-  var api = ionconnect.Api{ionconnect.GetConfig()}
+	var api = ionconnect.Api{ionconnect.GetConfig()}
 
-  app.Flags = []cli.Flag{
-    cli.BoolFlag{
-      Name:  "debug",
-      Usage: "display debug logging",
-    },
-  }
-  app.Before = func(c *cli.Context) error {
-    if c.Bool("debug") {
-      ionconnect.Debug = true
-      ionconnect.Debugln("Turning debug on.")
-    }
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "debug",
+			Usage: "display debug logging",
+		},
+	}
+	app.Before = func(c *cli.Context) error {
+		if c.Bool("debug") {
+			ionconnect.Debug = true
+			ionconnect.Debugln("Turning debug on.")
+		}
 
-    return nil
-  }
+		return nil
+	}
 
-  commands := make([]cli.Command,len(api.Config.Commands)+1 )
+	commands := make([]cli.Command, len(api.Config.Commands)+1)
 
-  for index, configCommand := range api.Config.Commands  {
-    subcommands := make([]cli.Command,len(configCommand.Subcommands))
-    for jndex, subcommand := range configCommand.Subcommands {
+	for index, configCommand := range api.Config.Commands {
+		subcommands := make([]cli.Command, len(configCommand.Subcommands))
+		for jndex, subcommand := range configCommand.Subcommands {
 
-      flags := make([]cli.Flag,len(subcommand.Flags))
-      for kndex, flag := range subcommand.Flags {
-        flags[kndex] = cli.StringFlag {
-          Name:        flag.Name,
-          Value:       flag.Value,
-          Usage:       flag.Usage,
-        }
-      }
+			flags := make([]cli.Flag, len(subcommand.Flags))
+			for kndex, flag := range subcommand.Flags {
+				flags[kndex] = cli.StringFlag{
+					Name:  flag.Name,
+					Value: flag.Value,
+					Usage: flag.Usage,
+				}
+			}
 
-      subcommands[jndex] = cli.Command{
-        Name:   subcommand.Name,
-        Usage:  subcommand.Usage,
-        Action: api.HandleCommand,
-        Flags: flags,
-      }
-    }
-    commands[index] = cli.Command{
-      Name:   configCommand.Name,
-      Usage:  configCommand.Usage,
-      Action: api.Noop,
-      Subcommands: subcommands,
-    }
-  }
+			subcommands[jndex] = cli.Command{
+				Name:   subcommand.Name,
+				Usage:  subcommand.Usage,
+				Action: api.HandleCommand,
+				Flags:  flags,
+			}
+		}
+		commands[index] = cli.Command{
+			Name:        configCommand.Name,
+			Usage:       configCommand.Usage,
+			Action:      api.Noop,
+			Subcommands: subcommands,
+		}
+	}
 
-  commands[len(commands)-1] = cli.Command{
-    Name:   "configure",
-    Usage:  "setup the Ion Channel secret key for later use",
-    Action: ionconnect.HandleConfigure,
-  }
+	commands[len(commands)-1] = cli.Command{
+		Name:   "configure",
+		Usage:  "setup the Ion Channel secret key for later use",
+		Action: ionconnect.HandleConfigure,
+	}
 
-  app.Commands = commands
+	app.Commands = commands
 
-
-  defer func() {
-    if r := recover(); r != nil {
-      fmt.Println(r)
-    }
-  }()
-  app.Run(os.Args)
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println(r)
+		}
+	}()
+	app.Run(os.Args)
 }
