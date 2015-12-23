da0845747e0a11d6c3100c39ce8919ddbb50d5f4
kellyp <kellyplummer@mac.com>
2015-12-23 15:53:14 -0800

    bumping version to 3.0

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "ion-connect"
 	app.Usage = "Interact with Ion Channel"
-	app.Version = "0.2.2"
+	app.Version = "0.3.0"
 
 	var api = ionconnect.Api{ionconnect.GetConfig()}
 
