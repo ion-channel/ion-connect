949bdebb1223383ae32cb6ca3526ea3d614d8b4c
kellyp <kellyplummer@mac.com>
2016-05-18 16:06:16 -0700

    adding animal commands and fmt

--- lib/config_test.go
+++ lib/config_test.go
@@ -8,187 +8,185 @@
 package ionconnect
 
 import (
-  . "github.com/onsi/ginkgo"
-  . "github.com/onsi/gomega"
-  "fmt"
-  "os"
+	"fmt"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"os"
 )
 
 var _ = Describe("Config", func() {
-  var (
-  )
+	var ()
 
-  BeforeEach(func() {
-    Debug = false
-    ION_HOME = fmt.Sprintf("%s/ionchannel-test", os.TempDir())
-    CREDENTIALS_FILE = fmt.Sprintf("%s/credentials", ION_HOME)
-  })
+	BeforeEach(func() {
+		Debug = false
+		ION_HOME = fmt.Sprintf("%s/ionchannel-test", os.TempDir())
+		CREDENTIALS_FILE = fmt.Sprintf("%s/credentials", ION_HOME)
+	})
 
-  Context("When the config.yaml file is loaded", func() {
-    config := GetConfig()
-    It("should contain an 'api' section", func() {
-        Expect(len(config.Commands)).To(Equal(5))
-    })
-    It("should contain the api version", func() {
-        Expect(config.Version).To(Equal("v1"))
-    })
-    It("should contain the api endpoint", func() {
-        Expect(config.Endpoint).To(Equal("https://api.ionchannel.io/"))
-    })
-    It("should contain the api token header name", func() {
-        Expect(config.Token).To(Equal("apikey"))
-    })
-    It("should have commands with subcommands", func() {
-        Expect(len(config.Commands[0].Subcommands)).To(Equal(8))
-        Expect(config.Commands[0].Subcommands[0].Name).To(Equal("scan-git"))
-        Expect(config.Commands[0].Subcommands[0].Post).To(BeTrue())
-    })
-    It("should have commands with subcommands and args", func() {
-        Expect(len(config.Commands[0].Subcommands)).To(Equal(8))
-        Expect(config.Commands[0].Subcommands[0].Name).To(Equal("scan-git"))
-        Expect(config.Commands[0].Subcommands[0].Args[0].Name).To(Equal("project"))
-    })
-  })
+	Context("When the config.yaml file is loaded", func() {
+		config := GetConfig()
+		It("should contain an 'api' section", func() {
+			Expect(len(config.Commands)).To(Equal(6))
+		})
+		It("should contain the api version", func() {
+			Expect(config.Version).To(Equal("v1"))
+		})
+		It("should contain the api endpoint", func() {
+			Expect(config.Endpoint).To(Equal("https://api.ionchannel.io/"))
+		})
+		It("should contain the api token header name", func() {
+			Expect(config.Token).To(Equal("apikey"))
+		})
+		It("should have commands with subcommands", func() {
+			Expect(len(config.Commands[0].Subcommands)).To(Equal(8))
+			Expect(config.Commands[0].Subcommands[0].Name).To(Equal("scan-git"))
+			Expect(config.Commands[0].Subcommands[0].Post).To(BeTrue())
+		})
+		It("should have commands with subcommands and args", func() {
+			Expect(len(config.Commands[0].Subcommands)).To(Equal(8))
+			Expect(config.Commands[0].Subcommands[0].Name).To(Equal("scan-git"))
+			Expect(config.Commands[0].Subcommands[0].Args[0].Name).To(Equal("project"))
+		})
+	})
 
-  Context("When processing a url", func() {
-    config := GetConfig()
-    It("it should render template code", func() {
-        params := GetParams{Id:"test"}
-        url, err := config.ProcessUrlFromConfig("metadata", "get-locations", params)
-        Expect(url).To(Equal("/metadata/getLocations"))
-        Expect(err).To(BeNil())
-    })
-    It("it should not fail if it's just a string", func() {
-        params := GetParams{Id:"test"}
-        url, err := config.ProcessUrlFromConfig("metadata", "get-locations", params)
-        Expect(url).To(Equal("/metadata/getLocations"))
-        Expect(err).To(BeNil())
-    })
-  })
+	Context("When processing a url", func() {
+		config := GetConfig()
+		It("it should render template code", func() {
+			params := GetParams{Id: "test"}
+			url, err := config.ProcessUrlFromConfig("metadata", "get-locations", params)
+			Expect(url).To(Equal("/metadata/getLocations"))
+			Expect(err).To(BeNil())
+		})
+		It("it should not fail if it's just a string", func() {
+			params := GetParams{Id: "test"}
+			url, err := config.ProcessUrlFromConfig("metadata", "get-locations", params)
+			Expect(url).To(Equal("/metadata/getLocations"))
+			Expect(err).To(BeNil())
+		})
+	})
 
-  Context("When we need the endpoint", func(){
-    config := GetConfig()
-    It("should read the endpoint from the config", func() {
-        Expect(config.LoadEndpoint()).To(Equal(config.Endpoint))
-    })
-    It("should read the endpoint from the config", func() {
-        Expect(config.LoadEndpoint()).To(Equal(config.Endpoint))
-        os.Setenv(ENDPOINT_ENVIRONMENT_VARIABLE, "numbersandletters")
-        Expect(config.LoadEndpoint()).To(Equal("numbersandletters"))
-        os.Unsetenv(ENDPOINT_ENVIRONMENT_VARIABLE)
-    })
+	Context("When we need the endpoint", func() {
+		config := GetConfig()
+		It("should read the endpoint from the config", func() {
+			Expect(config.LoadEndpoint()).To(Equal(config.Endpoint))
+		})
+		It("should read the endpoint from the config", func() {
+			Expect(config.LoadEndpoint()).To(Equal(config.Endpoint))
+			os.Setenv(ENDPOINT_ENVIRONMENT_VARIABLE, "numbersandletters")
+			Expect(config.LoadEndpoint()).To(Equal("numbersandletters"))
+			os.Unsetenv(ENDPOINT_ENVIRONMENT_VARIABLE)
+		})
 
-  })
+	})
 
+	Context("When we need creds", func() {
+		It("should create the ION_HOME directory", func() {
+			Expect(LoadCredential()).To(Equal(""))
+			Expect(PathExists(ION_HOME)).To(BeTrue())
+		})
+		It("should save credentials to new file", func() {
+			Expect(LoadCredential()).To(Equal(""))
+			Expect(PathExists(ION_HOME)).To(BeTrue())
+			Expect(PathExists(CREDENTIALS_FILE)).To(BeFalse())
+			Expect(func() { saveCredentials("notarealkey") }).ShouldNot(Panic())
+			Expect(PathExists(CREDENTIALS_FILE)).To(BeTrue())
+			Expect(LoadCredential()).To(Equal("notarealkey"))
+		})
+		It("should read credentials from and environment variable", func() {
+			os.Setenv(CREDENTIALS_ENVIRONMENT_VARIABLE, "numbersandletters")
+			Expect(LoadCredential()).To(Equal("numbersandletters"))
+			os.Unsetenv(CREDENTIALS_ENVIRONMENT_VARIABLE)
+		})
+	})
 
-  Context("When we need creds", func(){
-    It("should create the ION_HOME directory", func() {
-        Expect(LoadCredential()).To(Equal(""))
-        Expect(PathExists(ION_HOME)).To(BeTrue())
-    })
-    It("should save credentials to new file", func() {
-        Expect(LoadCredential()).To(Equal(""))
-        Expect(PathExists(ION_HOME)).To(BeTrue())
-        Expect(PathExists(CREDENTIALS_FILE)).To(BeFalse())
-        Expect(func(){saveCredentials("notarealkey")}).ShouldNot(Panic())
-        Expect(PathExists(CREDENTIALS_FILE)).To(BeTrue())
-        Expect(LoadCredential()).To(Equal("notarealkey"))
-    })
-    It("should read credentials from and environment variable", func() {
-      os.Setenv(CREDENTIALS_ENVIRONMENT_VARIABLE, "numbersandletters")
-      Expect(LoadCredential()).To(Equal("numbersandletters"))
-      os.Unsetenv(CREDENTIALS_ENVIRONMENT_VARIABLE)
-    })
-  })
+	Context("If we are looking for a command from the config", func() {
+		config := GetConfig()
+		It("should return the command config if found", func() {
+			command, err := config.FindCommandConfig("metadata")
+			Expect(command.Name).To(Equal("metadata"))
+			Expect(err).To(BeNil())
+		})
+		It("should return an error if not found", func() {
+			command, err := config.FindCommandConfig("not real")
+			Expect(string(err.Error())).To(Equal("Command not found"))
+			Expect(command.Name).To(Equal(""))
+		})
+	})
 
-  Context("If we are looking for a command from the config", func() {
-    config := GetConfig()
-    It("should return the command config if found", func() {
-      command, err := config.FindCommandConfig("metadata")
-      Expect(command.Name).To(Equal("metadata"))
-      Expect(err).To(BeNil())
-    })
-    It("should return an error if not found", func() {
-      command, err := config.FindCommandConfig("not real")
-      Expect(string(err.Error())).To(Equal("Command not found"))
-      Expect(command.Name).To(Equal(""))
-    })
-  })
+	Context("If we are looking for a subcommand from the config", func() {
+		config := GetConfig()
+		It("should return the subcommand config if found", func() {
+			command, err := config.FindSubCommandConfig("metadata", "get-locations")
+			Expect(command.Name).To(Equal("get-locations"))
+			Expect(err).To(BeNil())
+		})
+		It("should return an error if subcommand not found", func() {
+			command, err := config.FindSubCommandConfig("metadata", "not real")
+			Expect(string(err.Error())).To(Equal("Subcommand not found"))
+			Expect(command.Name).To(Equal(""))
+		})
+		It("should return an error if command not found", func() {
+			command, err := config.FindSubCommandConfig("not real", "not real")
+			Expect(string(err.Error())).To(Equal("Command not found"))
+			Expect(command.Name).To(Equal(""))
+		})
+	})
 
-  Context("If we are looking for a subcommand from the config", func() {
-    config := GetConfig()
-    It("should return the subcommand config if found", func() {
-      command, err := config.FindSubCommandConfig("metadata", "get-locations")
-      Expect(command.Name).To(Equal("get-locations"))
-      Expect(err).To(BeNil())
-    })
-    It("should return an error if subcommand not found", func() {
-      command, err := config.FindSubCommandConfig("metadata", "not real")
-      Expect(string(err.Error())).To(Equal("Subcommand not found"))
-      Expect(command.Name).To(Equal(""))
-    })
-    It("should return an error if command not found", func() {
-      command, err := config.FindSubCommandConfig("not real", "not real")
-      Expect(string(err.Error())).To(Equal("Command not found"))
-      Expect(command.Name).To(Equal(""))
-    })
-  })
+	Context("If we need to generate an argument string", func() {
+		Test = true
+		config := GetConfig()
+		It("should include all required args", func() {
+			command, err := config.FindSubCommandConfig("test", "test1")
+			Expect(err).To(BeNil())
+			Expect(len(command.Args)).To(Equal(2))
+			Expect(command.GetArgsUsage()).To(Equal("TEXT TEXT2 "))
+		})
+		It("should include optional args", func() {
+			command, err := config.FindSubCommandConfig("test", "test2")
+			Expect(err).To(BeNil())
+			Expect(len(command.Args)).To(Equal(3))
+			// probably not a good example, testing for order
+			Expect(command.GetArgsUsage()).To(Equal("TEXT [OTHERTEXT] TEXT2 "))
+		})
+		It("should be empty if no args", func() {
+			command, err := config.FindSubCommandConfig("test", "test3")
+			Expect(err).To(BeNil())
+			Expect(len(command.Args)).To(Equal(0))
+			Expect(command.GetArgsUsage()).To(Equal(""))
+		})
+	})
 
-  Context("If we need to generate an argument string", func() {
-    Test = true
-    config := GetConfig()
-    It("should include all required args", func() {
-      command, err := config.FindSubCommandConfig("test", "test1")
-      Expect(err).To(BeNil())
-      Expect(len(command.Args)).To(Equal(2))
-      Expect(command.GetArgsUsage()).To(Equal("TEXT TEXT2 "))
-    })
-    It("should include optional args", func() {
-      command, err := config.FindSubCommandConfig("test", "test2")
-      Expect(err).To(BeNil())
-      Expect(len(command.Args)).To(Equal(3))
-      // probably not a good example, testing for order
-      Expect(command.GetArgsUsage()).To(Equal("TEXT [OTHERTEXT] TEXT2 "))
-    })
-    It("should be empty if no args", func() {
-      command, err := config.FindSubCommandConfig("test", "test3")
-      Expect(err).To(BeNil())
-      Expect(len(command.Args)).To(Equal(0))
-      Expect(command.GetArgsUsage()).To(Equal(""))
-    })
-  })
+	Context("If a flag is supplied check for new args and apply", func() {
+		Test = true
+		config := GetConfig()
+		It("should include all required args", func() {
+			command, err := config.FindSubCommandConfig("test", "test1")
+			Expect(err).To(BeNil())
+			Expect(len(command.Flags)).To(Equal(2))
+			Expect(command.GetArgsUsage()).To(Equal("TEXT TEXT2 "))
 
-  Context("If a flag is supplied check for new args and apply", func() {
-    Test = true
-    config := GetConfig()
-    It("should include all required args", func() {
-      command, err := config.FindSubCommandConfig("test", "test1")
-      Expect(err).To(BeNil())
-      Expect(len(command.Flags)).To(Equal(2))
-      Expect(command.GetArgsUsage()).To(Equal("TEXT TEXT2 "))
+			Expect(command.GetArgsUsageWithFlags("project")).To(Equal("PROJECT "))
+		})
 
-      Expect(command.GetArgsUsageWithFlags("project")).To(Equal("PROJECT "))
-    })
+		It("should include all required args", func() {
+			command, err := config.FindSubCommandConfig("test", "test1")
+			Expect(err).To(BeNil())
+			Expect(len(command.Args)).To(Equal(2))
+			Expect(len(command.GetArgsForFlags("project"))).To(Equal(1))
+			Expect(command.GetArgsForFlags("project").GetRequiredArgsCount()).To(Equal(1))
+		})
 
-    It("should include all required args", func() {
-      command, err := config.FindSubCommandConfig("test", "test1")
-      Expect(err).To(BeNil())
-      Expect(len(command.Args)).To(Equal(2))
-      Expect(len(command.GetArgsForFlags("project"))).To(Equal(1))
-      Expect(command.GetArgsForFlags("project").GetRequiredArgsCount()).To(Equal(1))
-    })
+		It("should include all flags with args", func() {
+			command, err := config.FindSubCommandConfig("test", "test1")
+			Expect(err).To(BeNil())
+			Expect(len(command.Args)).To(Equal(2))
+			Expect(len(command.GetFlagsWithArgs())).To(Equal(2))
+			Expect(command.Args.GetRequiredArgsCount()).To(Equal(2))
+		})
+	})
 
-    It("should include all flags with args", func() {
-      command, err := config.FindSubCommandConfig("test", "test1")
-      Expect(err).To(BeNil())
-      Expect(len(command.Args)).To(Equal(2))
-      Expect(len(command.GetFlagsWithArgs())).To(Equal(2))
-      Expect(command.Args.GetRequiredArgsCount()).To(Equal(2))
-    })
-  })
-
-  AfterEach(func() {
-    Debug = false
-    os.RemoveAll(ION_HOME)
-  })
+	AfterEach(func() {
+		Debug = false
+		os.RemoveAll(ION_HOME)
+	})
 })
--- lib/params.go
+++ lib/params.go
@@ -8,126 +8,120 @@
 package ionconnect
 
 import (
-  // "github.com/ion-channel/ion-connect/Godeps/_workspace/src/github.com/codegangsta/cli"
-  "strings"
-  "reflect"
-  "fmt"
-  "io/ioutil"
-  "encoding/json"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"reflect"
+	"strings"
 )
 
 type Params interface {
 }
 
 type PostParams struct {
-    Project     string   `json:"project,omitempty"`
-    Url         string   `json:"url,omitempty"`
-    Type        string   `json:"type,omitempty"`
-    Checksum    string   `json:"checksum,omitempty"`
-    Id          string   `json:"id,omitempty"`
-    Text        string   `json:"text,omitempty"`
-    Version     string   `json:"version,omitempty"`
-    File        string   `json:"file,omitempty"`
-    ProjectId   string   `json:"project_id,omitempty"`
-    AccountId   string   `json:"account_id,omitempty"`
-    AnalysisId  string   `json:"analysis_id,omitempty"`
-    BuildNumber string   `json:"build_number,omitempty"`
-    Status      string   `json:"status,omitempty"`
-    Results     map[string]interface{}   `json:"results,omitempty"`
-    ScanType    string   `json:"scan_type,omitempty"`
-
-    Name    string   `json:"name,omitempty"`
-    Description string   `json:"description,omitempty"`
-    Rules []interface{}   `json:"rules,omitempty"`
-    // ScanSet map[string]interface{}   `json:"scan_set,omitempty"`
+	Project     string                 `json:"project,omitempty"`
+	Url         string                 `json:"url,omitempty"`
+	Type        string                 `json:"type,omitempty"`
+	Checksum    string                 `json:"checksum,omitempty"`
+	Id          string                 `json:"id,omitempty"`
+	Text        string                 `json:"text,omitempty"`
+	Version     string                 `json:"version,omitempty"`
+	File        string                 `json:"file,omitempty"`
+	ProjectId   string                 `json:"project_id,omitempty"`
+	AccountId   string                 `json:"account_id,omitempty"`
+	AnalysisId  string                 `json:"analysis_id,omitempty"`
+	BuildNumber string                 `json:"build_number,omitempty"`
+	Status      string                 `json:"status,omitempty"`
+	Results     map[string]interface{} `json:"results,omitempty"`
+	ScanType    string                 `json:"scan_type,omitempty"`
+	Name        string                 `json:"name,omitempty"`
+	Description string                 `json:"description,omitempty"`
+	Rules       []interface{}          `json:"rules,omitempty"`
 }
 
 type GetParams struct {
-    Project     string   `url:"project,omitempty"`
-    Url         string   `url:"url,omitempty"`
-    Type        string   `url:"type,omitempty"`
-    Checksum    string   `url:"checksum,omitempty"`
-    Id          string   `url:"id,omitempty"`
-    Text        string   `url:"text,omitempty"`
-    Version     string   `url:"version,omitempty"`
-    Limit       string   `url:"limit,omitempty"`
-    Offset      string   `url:"offset,omitempty"`
-    File        string   `url:"file,omitempty"`
-    ProjectId   string   `url:"project_id,omitempty"`
-    AccountId   string   `url:"account_id,omitempty"`
-    AnalysisId  string   `url:"analysis_id,omitempty"`
-    BuildNumber string   `url:"build_number,omitempty"`
-    Status      string   `url:"status,omitempty"`
-    Results     map[string]interface{}   `url:"results,omitempty"`
-    ScanType    string   `url:"scan_type,omitempty"`
-
-    Name    string   `url:"name,omitempty"`
-    Description string   `url:"description,omitempty"`
-    Rules []interface{}   `url:"rules,omitempty"`
-    // ScanSet map[string]interface{}   `url:"scan_set,omitempty"`
+	Project     string                 `url:"project,omitempty"`
+	Url         string                 `url:"url,omitempty"`
+	Type        string                 `url:"type,omitempty"`
+	Checksum    string                 `url:"checksum,omitempty"`
+	Id          string                 `url:"id,omitempty"`
+	Text        string                 `url:"text,omitempty"`
+	Version     string                 `url:"version,omitempty"`
+	Limit       string                 `url:"limit,omitempty"`
+	Offset      string                 `url:"offset,omitempty"`
+	File        string                 `url:"file,omitempty"`
+	ProjectId   string                 `url:"project_id,omitempty"`
+	AccountId   string                 `url:"account_id,omitempty"`
+	AnalysisId  string                 `url:"analysis_id,omitempty"`
+	BuildNumber string                 `url:"build_number,omitempty"`
+	Status      string                 `url:"status,omitempty"`
+	Results     map[string]interface{} `url:"results,omitempty"`
+	ScanType    string                 `url:"scan_type,omitempty"`
+	Name        string                 `url:"name,omitempty"`
+	Description string                 `url:"description,omitempty"`
+	Rules       []interface{}          `url:"rules,omitempty"`
 }
 
 func (params GetParams) String() string {
-  return fmt.Sprintf("Project=%s, Url=%s, Type=%s, Checksum=%s, Id=%s, Text=%s, Version=%s, Limit=%s, Offset=%s", params.Project, params.Url, params.Type, params.Checksum, params.Id, params.Text, params.Version, params.Limit, params.Offset)
+	return fmt.Sprintf("Project=%s, Url=%s, Type=%s, Checksum=%s, Id=%s, Text=%s, Version=%s, Limit=%s, Offset=%s", params.Project, params.Url, params.Type, params.Checksum, params.Id, params.Text, params.Version, params.Limit, params.Offset)
 }
 
 func (params GetParams) Generate(args []string, argConfigs []Arg) GetParams {
-  for index, arg := range args {
-    if argConfigs[index].Type != "object" &&  argConfigs[index].Type != "array" {
-      reflect.ValueOf(&params).Elem().FieldByName(strings.Replace(strings.Title(argConfigs[index].Name), "-", "", -1)).SetString(arg)
-    }
-  }
-  return params
+	for index, arg := range args {
+		if argConfigs[index].Type != "object" && argConfigs[index].Type != "array" {
+			reflect.ValueOf(&params).Elem().FieldByName(strings.Replace(strings.Title(argConfigs[index].Name), "-", "", -1)).SetString(arg)
+		}
+	}
+	return params
 }
 
 func (params GetParams) UpdateFromMap(paramMap map[string]string) GetParams {
-  for param, value := range paramMap {
-    Debugf("Setting %s to %s", strings.Replace(strings.Title(param), "-", "", -1), value )
-    reflect.ValueOf(&params).Elem().FieldByName(strings.Replace(strings.Title(param), "-", "", -1)).SetString(value)
-  }
-  return params
+	for param, value := range paramMap {
+		Debugf("Setting %s to %s", strings.Replace(strings.Title(param), "-", "", -1), value)
+		reflect.ValueOf(&params).Elem().FieldByName(strings.Replace(strings.Title(param), "-", "", -1)).SetString(value)
+	}
+	return params
 }
 
 func (params PostParams) String() string {
-  return fmt.Sprintf("Project=%s, Url=%s, Type=%s, Checksum=%s, Id=%s, Text=%s, Version=%s, File=%s", params.Project, params.Url, params.Type, params.Checksum, params.Id, params.Text, params.Version, params.File)
+	return fmt.Sprintf("Project=%s, Url=%s, Type=%s, Checksum=%s, Id=%s, Text=%s, Version=%s, File=%s", params.Project, params.Url, params.Type, params.Checksum, params.Id, params.Text, params.Version, params.File)
 }
 
 func (params PostParams) Generate(args []string, argConfigs []Arg) PostParams {
-  for index, arg := range args {
-    Debugf("Index and args %d %s %v", index, arg, argConfigs)
-    if argConfigs[index].Type == "file" {
-      Debugf("Reading file %s", arg)
-      bytes, err := ioutil.ReadFile(arg)
-      if err != nil {
-        panic(err.Error())
-      }
-      arg = string(bytes)
-    }
+	for index, arg := range args {
+		Debugf("Index and args %d %s %v", index, arg, argConfigs)
+		if argConfigs[index].Type == "file" {
+			Debugf("Reading file %s", arg)
+			bytes, err := ioutil.ReadFile(arg)
+			if err != nil {
+				panic(err.Error())
+			}
+			arg = string(bytes)
+		}
 
-    Debugf("Setting %s to %s", strings.Title(argConfigs[index].Name), arg )
-    if argConfigs[index].Type == "object" {
-      Debugln("Using object parser")
-      var jsonArg map[string]interface{}
-      err := json.Unmarshal([]byte(arg), &jsonArg)
-    	if err != nil {
-    		panic(fmt.Sprintf("Error parsing json from %s - %s", argConfigs[index].Name, err.Error()))
-    	}
-      reflect.ValueOf(&params).Elem().FieldByName(strings.Replace(strings.Title(argConfigs[index].Name), "-", "", -1)).Set(reflect.ValueOf(jsonArg))
-    } else if argConfigs[index].Type == "array" {
-      Debugln("Using array parser")
-      var jsonArray []interface{}
-      err := json.Unmarshal([]byte(arg), &jsonArray)
-    	if err != nil {
-    		panic(fmt.Sprintf("Error parsing json from %s - %s", argConfigs[index].Name, err.Error()))
-    	}
-      reflect.ValueOf(&params).Elem().FieldByName(strings.Replace(strings.Title(argConfigs[index].Name), "-", "", -1)).Set(reflect.ValueOf(jsonArray))
-    } else {
-      Debugln("Using string parser")
-      reflect.ValueOf(&params).Elem().FieldByName(strings.Replace(strings.Title(argConfigs[index].Name), "-", "", -1)).SetString(arg)
-    }
+		Debugf("Setting %s to %s", strings.Title(argConfigs[index].Name), arg)
+		if argConfigs[index].Type == "object" {
+			Debugln("Using object parser")
+			var jsonArg map[string]interface{}
+			err := json.Unmarshal([]byte(arg), &jsonArg)
+			if err != nil {
+				panic(fmt.Sprintf("Error parsing json from %s - %s", argConfigs[index].Name, err.Error()))
+			}
+			reflect.ValueOf(&params).Elem().FieldByName(strings.Replace(strings.Title(argConfigs[index].Name), "-", "", -1)).Set(reflect.ValueOf(jsonArg))
+		} else if argConfigs[index].Type == "array" {
+			Debugln("Using array parser")
+			var jsonArray []interface{}
+			err := json.Unmarshal([]byte(arg), &jsonArray)
+			if err != nil {
+				panic(fmt.Sprintf("Error parsing json from %s - %s", argConfigs[index].Name, err.Error()))
+			}
+			reflect.ValueOf(&params).Elem().FieldByName(strings.Replace(strings.Title(argConfigs[index].Name), "-", "", -1)).Set(reflect.ValueOf(jsonArray))
+		} else {
+			Debugln("Using string parser")
+			reflect.ValueOf(&params).Elem().FieldByName(strings.Replace(strings.Title(argConfigs[index].Name), "-", "", -1)).SetString(arg)
+		}
 
-
-    Debugf("Finished %s", arg)
-  }
-  return params
+		Debugf("Finished %s", arg)
+	}
+	return params
 }
