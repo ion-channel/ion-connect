6b33722d5e153eb7cf199c74ecf5d409da72a787
Kit Plummer <kitplummer@gmail.com>
2016-08-30 13:22:15 -0700

    adds ASL2.0 license and headers

--- lib/api.go
+++ lib/api.go
@@ -1,27 +1,37 @@
 // api.go
 //
-// Copyright (C) 2015 Selection Pressure LLC
+// Copyright [2016] [Selection Pressure]
 //
-// This software may be modified and distributed under the terms
-// of the MIT license.  See the LICENSE file for details.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package ionconnect
 
 import (
-	"encoding/json"
-	"github.com/codegangsta/cli"
-	"github.com/dghubble/sling"
-	"net/http"
 	"bytes"
 	"crypto/tls"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"mime/multipart"
+	"net/http"
 	"os"
 	"strings"
+
+	"github.com/codegangsta/cli"
+	"github.com/dghubble/sling"
 )
 
 type Api struct {
@@ -92,7 +102,6 @@ func (api Api) sendRequest(command string, subcommand string, context *cli.Conte
 		client.QueryStruct(&params)
 		Debugf("Sending params %b", params)
 	}
-
 
 	url, err := api.Config.ProcessUrlFromConfig(command, subcommand, GetParams{}.Generate(context.Args(), args))
 	if err != nil {
