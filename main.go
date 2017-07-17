7d9fa7aa1e2c153a5438ff1535cf3dcb68d43257
kellyp <kellyplummer@mac.com>
2017-07-17 12:02:20 -0700

    removing the commands for file scanning

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func getApp() *cli.App {
 	app := cli.NewApp()
 	app.Name = "ion-connect"
 	app.Usage = "Interact with Ion Channel"
-	app.Version = "0.10.2"
+	app.Version = "0.11.0"
 	return app
 }
 
