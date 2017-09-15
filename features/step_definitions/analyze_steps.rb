a825eefa2f1c383e48727299ef6a181548427a1f
Daniel Hess <dan9186@gmail.com>
2017-09-15 12:01:23 -0700

    updating analysis output to reflect what we will see

--- features/step_definitions/analyze_steps.rb
+++ features/step_definitions/analyze_steps.rb
@@ -11,10 +11,11 @@ end
 # Thens
 
 Then(/^I see a response showing the project is analyzed$/) do
-  expect(@output).to include('Finished about_yml scan for java-lew, valid .about.yml found.')
-  expect(@output).to include('Compliance analysis completed successfully')
-  expect(@output).to include(@branch)
-  expect(@output).to include('is compliant!')
+  expect(@output).to include("Begining compliance analysis of project (#{@project_id})")
+  expect(@output).to include('Analysis requested the id is')
+  expect(@output).to include('All project scans have finished')
+  expect(@output).to include('Evaluating analysis for compliance')
+  expect(@output).to include('Compliance analysis failed, your project is not compliant')
 
   # Only so the `Given previous output` step works. Once those are gone, remove this.
   $output = @output
