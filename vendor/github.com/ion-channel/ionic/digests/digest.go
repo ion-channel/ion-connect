df81bacea9a47ea76b71e7085fa8b4c840c97e98
kellyp <kellyplummer@mac.com>
2019-02-15 16:04:55 -0800

    add ability to command the search endpoint

--- vendor/github.com/ion-channel/ionic/digests/digest.go
+++ vendor/github.com/ion-channel/ionic/digests/digest.go
@@ -6,6 +6,7 @@ import (
 	"math"
 	"strings"
 
+	"github.com/ion-channel/ionic/scanner"
 	"github.com/ion-channel/ionic/scans"
 )
 
@@ -36,6 +37,9 @@ type Digest struct {
 	WarningMessage string          `json:"warning_message,omitempty"`
 	Errored        bool            `json:"errored"`
 	ErroredMessage string          `json:"errored_message,omitempty"`
+
+	singularTitle string
+	pluralTitle   string
 }
 
 type boolean struct {
@@ -58,70 +62,102 @@ type percent struct {
 	Percent float64 `json:"percent"`
 }
 
-// NewFromEval takes a title, data type and value to attempt constructing a digest. It
-// returns a digest and any error that it encounters while trying to construct
-// the digest.
-func NewFromEval(index int, title string, dataType string, value interface{}, eval *scans.Evaluation) (*Digest, error) {
+// NewDigest takes a scan status, ordering index, and a singular and plural
+// version of the digest title. The status is leveraged to determine the first
+// levels of a digest status. The index provides an ordered location if a list
+// of Digests are sorted. The singular and plural titles are used to display as
+// singular title if data is added that warrants a singular title, and otherwise
+// will favor a plural title. A newly constructed Digest with the appropriate
+// settings is returned.
+func NewDigest(status *scanner.ScanStatus, index int, singular, plural string) *Digest {
+	var errored bool
+	var erroredMsg string
+
+	if status != nil && strings.ToLower(status.Status) == "errored" {
+		errored = true
+		erroredMsg = status.Message
+	}
+
+	d := &Digest{
+		Index:          index,
+		Pending:        status == nil,
+		Errored:        errored,
+		ErroredMessage: erroredMsg,
+		Title:          plural,
+		singularTitle:  singular,
+		pluralTitle:    plural,
+	}
+
+	return d
+}
+
+// AppendEval takes an evaluation, data type, and value to interleave into the
+// digest. It will also use the data to determine whether or not to show it's
+// singular or plural title. It returns an error if the data type does not match
+// the value provided.
+func (d *Digest) AppendEval(eval *scans.Evaluation, dataType string, value interface{}) error {
 	var data []byte
 	var err error
+	title := d.pluralTitle
 
 	switch strings.ToLower(dataType) {
 	case "bool", "boolean":
 		b, ok := value.(bool)
 		if !ok {
-			return nil, ErrFailedValueAssertion
+			return ErrFailedValueAssertion
 		}
 
 		data, err = json.Marshal(boolean{b})
 	case "chars":
 		c, ok := value.(string)
 		if !ok {
-			return nil, ErrFailedValueAssertion
+			return ErrFailedValueAssertion
 		}
 
 		data, err = json.Marshal(chars{c})
 	case "count":
 		c, ok := value.(int)
 		if !ok {
-			return nil, ErrFailedValueAssertion
+			return ErrFailedValueAssertion
 		}
 
 		data, err = json.Marshal(count{c})
+		if c == 1 {
+			title = d.singularTitle
+		}
 	case "list":
 		l, ok := value.([]string)
 		if !ok {
-			return nil, ErrFailedValueAssertion
+			return ErrFailedValueAssertion
 		}
 
 		data, err = json.Marshal(&list{l})
+		if len(l) == 1 {
+			title = d.singularTitle
+		}
 	case "percent":
 		p, ok := value.(float64)
 		if !ok {
-			return nil, ErrFailedValueAssertion
+			return ErrFailedValueAssertion
 		}
 
 		data, err = json.Marshal(percent{math.Round(p*100) / 100})
 	default:
-		return nil, ErrUnsupportedType
+		return ErrUnsupportedType
 	}
 
 	if err != nil {
-		return nil, fmt.Errorf("failed to marshal digest data: %v", err.Error())
+		return fmt.Errorf("failed to marshal digest data: %v", err.Error())
 	}
 
-	d := &Digest{
-		Index: index,
-		Title: title,
-		Data:  data,
+	d.Data = data
+	d.Title = title
+	d.ScanID = eval.ID
+	d.RuleID = eval.RuleID
+	d.RulesetID = eval.RulesetID
+	d.Evaluated = (strings.ToLower(eval.Type) != "not evaluated")
+	d.Passed = eval.Passed
+	d.PassedMessage = eval.Description
 
-		ScanID:    eval.ID,
-		RuleID:    eval.RuleID,
-		RulesetID: eval.RulesetID,
-
-		Evaluated:     (strings.ToLower(eval.Type) != "not evaluated"),
-		Passed:        eval.Passed,
-		PassedMessage: eval.Description,
-	}
-
-	return d, nil
+	return nil
 }
--- vendor/github.com/ion-channel/ionic/digests/digest_test.go
+++ vendor/github.com/ion-channel/ionic/digests/digest_test.go
@@ -4,6 +4,7 @@ import (
 	"sort"
 	"testing"
 
+	"github.com/ion-channel/ionic/scanner"
 	"github.com/ion-channel/ionic/scans"
 
 	"github.com/franela/goblin"
@@ -15,37 +16,6 @@ func TestDigest(t *testing.T) {
 	RegisterFailHandler(func(m string, _ ...int) { g.Fail(m) })
 
 	g.Describe("Digest", func() {
-		g.Describe("Constructor", func() {
-			g.It("should return an error for an unsupported digest type", func() {
-				e := scans.NewEval()
-				ds, err := NewFromEval(0, "A title", "badtype", 10, e)
-				Expect(err).To(Equal(ErrUnsupportedType))
-				Expect(ds).To(BeNil())
-			})
-
-			g.It("should return an error when the value doesn't match the type", func() {
-				e := scans.NewEval()
-				ds, err := NewFromEval(0, "Another title", "bool", "not a bool", e)
-				Expect(err).To(Equal(ErrFailedValueAssertion))
-				Expect(ds).To(BeNil())
-
-				e = scans.NewEval()
-				ds, err = NewFromEval(1, "Another title", "count", "not a count", e)
-				Expect(err).To(Equal(ErrFailedValueAssertion))
-				Expect(ds).To(BeNil())
-
-				e = scans.NewEval()
-				ds, err = NewFromEval(2, "Another title", "list", "not a list", e)
-				Expect(err).To(Equal(ErrFailedValueAssertion))
-				Expect(ds).To(BeNil())
-
-				e = scans.NewEval()
-				ds, err = NewFromEval(3, "Another title", "percent", "not a percent", e)
-				Expect(err).To(Equal(ErrFailedValueAssertion))
-				Expect(ds).To(BeNil())
-			})
-		})
-
 		g.Describe("Sorting", func() {
 			g.It("should be sortable by index", func() {
 				ds := []Digest{
@@ -66,6 +36,145 @@ func TestDigest(t *testing.T) {
 				Expect(ds[1].Index).To(Equal(1))
 				Expect(ds[2].Index).To(Equal(2))
 				Expect(ds[3].Index).To(Equal(3))
+			})
+		})
+
+		g.Describe("States", func() {
+			g.It("should be marked as pending when no status is provided", func() {
+				ds := NewDigest(nil, 0, "", "")
+				Expect(ds).NotTo(BeNil())
+				Expect(ds.Pending).To(BeTrue())
+				Expect(ds.Errored).To(BeFalse())
+			})
+
+			g.It("should show an error if present", func() {
+				s := &scanner.ScanStatus{
+					Status:  "errored",
+					Message: "failed to perform the scan for a reason",
+				}
+				ds := NewDigest(s, 0, "", "")
+				Expect(ds).NotTo(BeNil())
+				Expect(ds.Pending).To(BeFalse())
+				Expect(ds.Errored).To(BeTrue())
+				Expect(ds.ErroredMessage).To(Equal("failed to perform the scan for a reason"))
+
+				s = &scanner.ScanStatus{
+					Status:  "finished",
+					Message: "completed scan",
+				}
+				ds = NewDigest(s, 0, "", "")
+				Expect(ds).NotTo(BeNil())
+				Expect(ds.Pending).To(BeFalse())
+				Expect(ds.Errored).To(BeFalse())
+				Expect(ds.ErroredMessage).To(Equal(""))
+			})
+		})
+
+		g.Describe("Pluralization", func() {
+			g.It("should return a singular title when appropriate", func() {
+				ds := Digest{
+					singularTitle: "vulnerability",
+					pluralTitle:   "vulnerabilities",
+				}
+				e := scans.NewEval()
+
+				err := ds.AppendEval(e, "count", 1)
+				Expect(err).To(BeNil())
+				Expect(ds.Title).To(Equal("vulnerability"))
+
+				err = ds.AppendEval(e, "list", []string{"something"})
+				Expect(err).To(BeNil())
+				Expect(ds.Title).To(Equal("vulnerability"))
+
+				err = ds.AppendEval(e, "chars", "true")
+				Expect(err).To(BeNil())
+				Expect(ds.Title).To(Equal("vulnerabilities"))
+
+				err = ds.AppendEval(e, "bool", true)
+				Expect(err).To(BeNil())
+				Expect(ds.Title).To(Equal("vulnerabilities"))
+
+				err = ds.AppendEval(e, "percent", float64(10))
+				Expect(err).To(BeNil())
+				Expect(ds.Title).To(Equal("vulnerabilities"))
+			})
+
+			g.It("should return a plural title when appropriate", func() {
+				ds := Digest{
+					singularTitle: "vulnerability",
+					pluralTitle:   "vulnerabilities",
+				}
+				e := scans.NewEval()
+
+				err := ds.AppendEval(e, "count", 10)
+				Expect(err).To(BeNil())
+				Expect(ds.Title).To(Equal("vulnerabilities"))
+
+				err = ds.AppendEval(e, "list", []string{"something", "another"})
+				Expect(err).To(BeNil())
+				Expect(ds.Title).To(Equal("vulnerabilities"))
+
+				err = ds.AppendEval(e, "chars", "true")
+				Expect(err).To(BeNil())
+				Expect(ds.Title).To(Equal("vulnerabilities"))
+
+				err = ds.AppendEval(e, "bool", true)
+				Expect(err).To(BeNil())
+				Expect(ds.Title).To(Equal("vulnerabilities"))
+
+				err = ds.AppendEval(e, "percent", float64(10))
+				Expect(err).To(BeNil())
+				Expect(ds.Title).To(Equal("vulnerabilities"))
+			})
+		})
+
+		g.Describe("Evaluation", func() {
+			g.It("should return an error for an unsupported digest type", func() {
+				ds := Digest{}
+				e := scans.NewEval()
+
+				err := ds.AppendEval(e, "badtype", 10)
+				Expect(err).To(Equal(ErrUnsupportedType))
+			})
+
+			g.It("should return an error when the value doesn't match the type", func() {
+				ds := Digest{}
+				e := scans.NewEval()
+
+				err := ds.AppendEval(e, "bool", "not a bool")
+				Expect(err).To(Equal(ErrFailedValueAssertion))
+
+				err = ds.AppendEval(e, "count", "not a count")
+				Expect(err).To(Equal(ErrFailedValueAssertion))
+
+				err = ds.AppendEval(e, "list", "not a list")
+				Expect(err).To(Equal(ErrFailedValueAssertion))
+
+				err = ds.AppendEval(e, "percent", "not a percent")
+				Expect(err).To(Equal(ErrFailedValueAssertion))
+
+				err = ds.AppendEval(e, "chars", true)
+				Expect(err).To(Equal(ErrFailedValueAssertion))
+			})
+
+			g.It("should append added evaluation info", func() {
+				ds := &Digest{}
+				e := scans.NewEval()
+
+				e.ID = "someevalid"
+				e.RuleID = "someruleid"
+				e.RulesetID = "somerulesetid"
+				e.Type = "someeval"
+				e.Passed = true
+				e.Description = "we evaluated a thing"
+
+				err := ds.AppendEval(e, "count", 10)
+				Expect(err).To(BeNil())
+				Expect(ds.ScanID).To(Equal("someevalid"))
+				Expect(ds.RuleID).To(Equal("someruleid"))
+				Expect(ds.RulesetID).To(Equal("somerulesetid"))
+				Expect(ds.Evaluated).To(BeTrue())
+				Expect(ds.Passed).To(BeTrue())
 			})
 		})
 	})
