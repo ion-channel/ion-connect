14bb75fcbb814c29fa6468ab34a01135a12eaf45
kellyp <kellyplummer@mac.com>
2020-01-23 11:19:04 -0800

    Add command for creation of single projects

--- cmd/project.go
+++ cmd/project.go
@@ -1,9 +1,12 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 
 	"github.com/ion-channel/ionic/pagination"
+	"github.com/ion-channel/ionic/projects"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -13,6 +16,7 @@ func init() {
 	ProjectCmd.AddCommand(GetProjectCmd)
 	ProjectCmd.AddCommand(GetProjectsCmd)
 	ProjectCmd.AddCommand(CreateProjectsCSVCmd)
+	ProjectCmd.AddCommand(CreateProjectCmd)
 
 	GetProjectCmd.Flags().StringVarP(&teamID, "team-id", "t", "", "ID of the team for the project (required)")
 	GetProjectCmd.MarkFlagRequired("team-id")
@@ -26,6 +30,8 @@ func init() {
 
 	CreateProjectsCSVCmd.Flags().StringVarP(&teamID, "team-id", "t", "", "ID of the team for the project (required)")
 	CreateProjectsCSVCmd.MarkFlagRequired("team-id")
+
+	CreateProjectCmd.Flags().BoolVar(&skel, "print", false, "Print an example create project json skeleton")
 }
 
 // ProjectCmd - Container for holding project root and secondary commands
@@ -57,7 +63,7 @@ var GetProjectsCmd = &cobra.Command{
 	Long:  `Get the data for a projects in a team`,
 	Run: func(cmd *cobra.Command, args []string) {
 		page := pagination.New(limit, offset)
-		ps, e := ion.GetProjects(teamID, viper.GetString(secretKey), page)
+		ps, e := ion.GetProjects(teamID, viper.GetString(secretKey), page, nil)
 		if e != nil {
 			fmt.Println(e.Error())
 		}
@@ -80,5 +86,69 @@ var CreateProjectsCSVCmd = &cobra.Command{
 		}
 
 		PPrint(ps)
+	},
+}
+
+// CreateProjectCmd - Container for holding create project root and secondary commands
+var CreateProjectCmd = &cobra.Command{
+	Use:   "create-project [flags] PATHTOJSON",
+	Short: "Create Project",
+	Long:  `Create project from a Ion Channel JSON input file`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if skel {
+			return nil
+		}
+		if len(args) != 1 {
+			return fmt.Errorf("accepts 1 arg(s), received 0")
+		}
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		if skel {
+			fmt.Println("{")
+			fmt.Println("   \"team_id\":\"\",")
+			fmt.Println("   \"ruleset_id\":\"\",")
+			fmt.Println("   \"name\":\"\",")
+			fmt.Println("   \"type\":\"\",")
+			fmt.Println("   \"source\":\"\",")
+			fmt.Println("   \"description\":\"\",")
+			fmt.Println("   \"username\":\"\",")
+			fmt.Println("   \"password\":\"\"")
+			fmt.Println("}")
+			return
+		}
+
+		filename := args[0]
+
+		f, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		var data projects.Project
+		err = json.Unmarshal(f, &data)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		errs, err := data.Validate(cc, uu, viper.GetString(secretKey))
+		if err != nil {
+			for name, e := range errs {
+				fmt.Printf("%v : %v\n", name, e)
+			}
+
+			fmt.Println(err.Error())
+			return
+		}
+
+		r, err := ion.CreateProject(&data, teamID, viper.GetString(secretKey))
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		PPrint(r)
 	},
 }
