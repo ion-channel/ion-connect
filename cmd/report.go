21de273b88b4899b0133a12ea80a693ac3d595be
Brent Johnson <brentj433@gmail.com>
2020-11-12 11:26:48 -0800

    adds data export

--- cmd/report.go
+++ cmd/report.go
@@ -1,8 +1,11 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 
+	"github.com/ion-channel/ionic/requests"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -10,11 +13,14 @@ import (
 func init() {
 	RootCmd.AddCommand(ReportCmd)
 	ReportCmd.AddCommand(GetAnalysisReportCmd)
+	ReportCmd.AddCommand(GetExportData)
 
 	GetAnalysisReportCmd.Flags().StringVarP(&teamID, "team-id", "t", "", "ID of the team for the project (required)")
 	GetAnalysisReportCmd.MarkFlagRequired("team-id")
 	GetAnalysisReportCmd.Flags().StringVarP(&projectID, "project-id", "p", "", "ID of the project (required)")
 	GetAnalysisReportCmd.MarkFlagRequired("project-id")
+
+	GetExportData.Flags().BoolVar(&skel, "print", false, "Print an example export data json skeleton. ids is 1-n project ids.")
 
 }
 
@@ -39,5 +45,70 @@ var GetAnalysisReportCmd = &cobra.Command{
 		}
 
 		PPrint(a)
+	},
+}
+
+// GetExportData - Container for holding get exported data root and secondary commands
+var GetExportData = &cobra.Command{
+	Use:   "export-data [flags] PATHTOJSON",
+	Short: "Export Data",
+	Long:  `Export Data - requires team id and project id(s) from a json file`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if skel {
+			return nil
+		}
+		if len(args) != 1 {
+			return fmt.Errorf("accepts 1 arg(s), received 0")
+		}
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		if skel {
+			fmt.Println("{")
+			fmt.Println("   \"team_id\":\"\",")
+			fmt.Println("   \"ids\": [")
+			fmt.Println("      \"uuid1\",")
+			fmt.Println("      \"\",")
+			fmt.Println("      \"\",")
+			fmt.Println("      \"\",")
+			fmt.Println("      \"\",")
+			fmt.Println("      \"\"")
+			fmt.Println("   ]")
+			fmt.Println("}")
+			return
+		}
+
+		filename := args[0]
+
+		f, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		var data requests.ByIDsAndTeamID
+		err = json.Unmarshal(f, &data)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		if data.TeamID == "" {
+			fmt.Println("team_id is required")
+			return
+		}
+
+		if len(data.IDs) == 0 {
+			fmt.Println("one or more project ids are required in ids json filed")
+			return
+		}
+
+		r, err := ion.GetExportedProjectsData(data.IDs, data.TeamID, viper.GetString(secretKey))
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		PPrint(r)
 	},
 }
